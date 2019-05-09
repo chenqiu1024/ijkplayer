31cbcc73e1be54eadf202bba5a758e04535a1063
chenqiu1024 <godspeed1024@gmail.com>
2019-05-09 11:39:25 +0800

    Lower volume;

--- ios/IJKMediaPlayer/IJKMediaPlayer/ijkmedia/ijksdl/ios/IJKSDLAudioUnitController.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/ijkmedia/ijksdl/ios/IJKSDLAudioUnitController.m
@@ -110,7 +110,7 @@
         AUGraphAddNode(_auGraph, &spliterACDesc, &spliterNode);
         AUGraphConnectNodeInput(_auGraph, ioNode, 1, mixerNode, 1);
         AUGraphConnectNodeInput(_auGraph, resampleNode, 0, mixerNode, 0);
-        AUGraphConnectNodeInput(_auGraph, mixerNode, 0, ioNode, 0);
+//        AUGraphConnectNodeInput(_auGraph, mixerNode, 0, ioNode, 0);
         
         AUGraphOpen(_auGraph);
         
@@ -235,8 +235,8 @@
 //        mixerRenderCallback.inputProcRefCon = (__bridge void*) self;
         
 //        status = AudioUnitSetProperty(_ioUnit, kAudioOutputUnitProperty_SetInputCallback, kAudioUnitScope_Global, 0, &micInputCallback, sizeof(micInputCallback));
-//        status = AudioUnitAddRenderNotify(_ioUnit, (AURenderCallback)MicInputCallback, (__bridge void*)self);
-//        NSLog(@"#RecordCallback#AudioUnitCallback# status=%d, at %d in %s", status, __LINE__, __PRETTY_FUNCTION__);
+        status = AudioUnitAddRenderNotify(_ioUnit, (AURenderCallback)MicInputCallback, (__bridge void*)self);
+        NSLog(@"#RecordCallback#AudioUnitCallback# status=%d, at %d in %s", status, __LINE__, __PRETTY_FUNCTION__);
         
         status = AudioUnitAddRenderNotify(_mixerUnit, (AURenderCallback)MixerRenderNotifyCallback, (__bridge void*)self);
         NSLog(@"#RecordCallback#AudioUnitCallback# status=%d, at %d in %s", status, __LINE__, __PRETTY_FUNCTION__);
@@ -507,17 +507,18 @@ static OSStatus MixerRenderNotifyCallback(void                        *inRefCon,
                 NSLog(@"#AudioUnitCallback# MixerRenderNotifyCallback : audioBuffer[%d].channels=%d, .size=%d, .data=0x%lx", i, audioBuffer.mNumberChannels, audioBuffer.mDataByteSize, (long)audioBuffer.mData);
                 if (audioBuffer.mData && (*ioActionFlags & kAudioUnitRenderAction_PostRender))
                 {//PeriodInSamples = SampleRate / Frequency = 1024 / k, k = 1024 * Frequency / SampleRate
-                    const float F0 = 430.7, F1 = 861.4, A0 = 0.15, A1 = 0.15;
+                    const float F0 = 430.7, F1 = 861.4, A0 = 0.05, A1 = 0.05;
                     static NSUInteger totalSamples = 0;
                     ushort* pDst = (ushort*)audioBuffer.mData;
-                    for (int iSample=0; iSample<inNumberFrames; ++iSample)
-                    {
-                        totalSamples++;
-                        float a0 = sinf(2 * M_PI * F0 * totalSamples / 44100.f) * A0;
-                        *(pDst++) = 32768 * a0 + 32767;
-                        float a1 = sinf(2 * M_PI * F1 * totalSamples / 44100.f) * A1;
-                        *(pDst++) = 32768 * a1 + 32767;
-                    }
+//                    for (int iSample=0; iSample<inNumberFrames; ++iSample)
+//                    for (int iSample=0; iSample<audioBuffer.mDataByteSize/4; ++iSample)
+//                    {
+//                        totalSamples++;
+//                        float a0 = sinf(2 * M_PI * F0 * totalSamples / 44100.f) * A0;
+//                        *(pDst++) = 32768 * a0 + 32767;
+//                        float a1 = sinf(2 * M_PI * F1 * totalSamples / 44100.f) * A1;
+//                        *(pDst++) = 32768 * a1 + 32767;
+//                    }
 //                    memset(audioBuffer.mData, 0, audioBuffer.mDataByteSize);///!!!
                 }
             }
@@ -543,10 +544,10 @@ static OSStatus MixerRenderNotifyCallback(void                        *inRefCon,
 //
         if (!ioData)
             return noErr;
-//        if (*ioActionFlags & kAudioUnitRenderAction_PreRender)
-//            return noErr;
-//        if (!(*ioActionFlags & kAudioUnitRenderAction_PostRender))
-//            return noErr;
+        if (*ioActionFlags & kAudioUnitRenderAction_PreRender)
+            return noErr;
+        if (!(*ioActionFlags & kAudioUnitRenderAction_PostRender))
+            return noErr;
         
         static NSUInteger totalDataSize = 0;
         static NSDate* beginTime;
